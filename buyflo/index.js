73cb22da5506ea8c684a567c1b373e0a8276efa3
kala Asia <161475983+melodymala@users.noreply.github.com>
2024-03-17 01:34:45 -0700

    Add files via upload

---
+++ buyflo/index.js
@@ -0,0 +1,42 @@
+
+let tulipBtnOne=document.getElementById("tulip-btn-one")
+let tulipBtnTwo=document.getElementById("tulip-btn-two")
+let tulipBtnThree=document.getElementById("tulip-btn-three")
+
+let tulipEl=document.getElementById("gettulip")
+let tulip=0
+function TulipOne(){
+tulip+=1
+tulipEl.textContent=tulip
+
+}
+function TulipTwo(){
+    tulip+=2
+    tulipEl.textContent=tulip
+    
+    }
+    function TulipThree(){
+        tulip+=3
+        tulipEl.textContent=tulip
+        
+        }
+let roseBtnOne=document.getElementById("rose-btn-one")
+let roseBtnTwo=document.getElementById("rose-btn-two")
+let roseBtnThree=document.getElementById("rose-btn-three")
+let roseEl=document.getElementById("getrose")
+let rose=0
+function roseOne(){
+rose+=1
+roseEl.textContent=rose
+
+}
+function roseTwo(){
+    rose+=2
+    roseEl.textContent=rose
+    
+    }
+    function roseThree(){
+        rose+=3
+        roseEl.textContent=rose
+        
+        }
